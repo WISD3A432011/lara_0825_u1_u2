2e39e89dfef67f5631b9eb17a22e6b9b6d20ac8c
CHIA-YU-TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-26 19:20:56 +0800

    練習六-3修改

--- database/migrations/2017_10_26_111236_create_article_table.php
+++ database/migrations/2017_10_26_111236_create_article_table.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-class Article extends Migration
+class CreateArticleTable extends Migration
 {
     /**
      * Run the migrations.
