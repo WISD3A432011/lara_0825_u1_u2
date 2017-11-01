a462d123b7ccf5bb6dce3e5cd2ebaf91e5e941a1
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-01 17:52:33 +0800

    修改change(),after()指令

--- database/migrations/2017_10_26_112306_add_is_feature_in_posts_table.php
+++ database/migrations/2017_10_26_112306_add_is_feature_in_posts_table.php
@@ -15,8 +15,8 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
-            $table ‐>string('title',500) ‐>change();
-            $table ‐>boolean('is_feature') ‐>default(false) ‐>after('content');
+            $table ->string('title',500) ->change();
+            $table ->boolean('is_feature') ->default(false) ->after('content');
         });
     }
 
