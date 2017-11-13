a116c3320b72582256f3c9ae42a2f1971eebb309
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-13 18:15:34 +0800

    單元一練習6-1： 取得Collection

--- routes/web.php
+++ routes/web.php
@@ -63,5 +63,11 @@ Route::get('/test', function () {
 /*
     \App\Post::destroy(2);
 */
+/*
     \App\Post::destroy(3,5,7);
+*/
+    $allPosts = \App\Post::all();
+    dd($allPosts);
+    $featuredPosts= \App\Post::where('is_feature',1)->get();
+    dd($featuredPosts);
 });
