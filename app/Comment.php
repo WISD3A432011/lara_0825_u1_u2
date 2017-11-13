1c221a8753cc286581a176070a4e364bab8f7485
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-13 19:26:05 +0800

    單元一練習7-3：開啟Comment.php，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Post;
 class Comment extends Model
 {
-    //
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
