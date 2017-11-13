bcc89dfe4e43d53998d5ea1dcfb728c24a8dd7c9
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-13 19:44:59 +0800

    修正Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use App\Post;
 class Comment extends Model
 {
+    protected $table = 'comments';
+    protected $fillable=['title', 'content_2', 'is_feature'];
     public function post()
     {
         return $this->belongsTo(Post::class);
