90fbeecf1149f8c33247e31aa749a0aae3c8b343
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-20 16:39:24 +0800

    單元二練習4-1：每次 seeding 前先清空資料表

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -10,6 +10,8 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+        Post::truncate();
+
         //單元二-1
         foreach(range(1,20) as $number) {
             Post::create([
